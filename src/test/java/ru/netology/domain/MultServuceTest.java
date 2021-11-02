af1516fb3a959b699b5e4759c8e8dfd9a9a48eae
Khvostenko <khmark04@mail.ru>
2021-11-03 02:48:32 +0700

    build succes

--- src/test/java/ru/netology/domain/MultServuceTest.java
+++ src/test/java/ru/netology/domain/MultServuceTest.java
@@ -22,6 +22,16 @@ class MultServiceTest {
     public void switchStationNext(){
         Radio rad = new Radio();
 
+        rad.currentStation = 6;
+        int expected = 7;
+        int actual = rad.switchStationNext();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void switchStationNext2(){
+        Radio rad = new Radio();
+
         rad.currentStation = 9;
         int expected = 0;
         int actual = rad.switchStationNext();
@@ -31,6 +41,16 @@ class MultServiceTest {
 
     @Test
     public void switchStationPrev(){
+        Radio rad = new Radio();
+
+        rad.currentStation = 5;
+        int expected = 4;
+        int actual = rad.switchStationPrev();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void switchStationPrev2(){
         Radio rad = new Radio();
 
         rad.currentStation = 0;
@@ -43,6 +63,16 @@ class MultServiceTest {
     public void increaseVolume(){
         Radio rad = new Radio();
 
+        rad.currentVolume = 5;
+        int expected = 6;
+        int actual = rad.increaseVolume();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void increaseVolume2(){
+        Radio rad = new Radio();
+
         rad.currentVolume = 10;
         int expected = 10;
         int actual = rad.increaseVolume();
@@ -51,6 +81,16 @@ class MultServiceTest {
 
     @Test
     public void lowerVolume(){
+        Radio rad = new Radio();
+
+        rad.currentVolume = 5;
+        int expected = 4;
+        int actual = rad.lowerVolume();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void lowerVolume2(){
         Radio rad = new Radio();
 
         rad.currentVolume = 0;
